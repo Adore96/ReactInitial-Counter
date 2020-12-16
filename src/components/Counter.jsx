32fd145878c6d150beb026718b345946944ecfe6
Kalhara Perera <adorekasun@gmail.com>
2020-12-16 23:30:07 +0530

    JSX , Rendering List , Conditional Rendering , Handling events , Updating the state :100:  Finished

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,8 +6,6 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  // check
-
   styles = {
     fontSize: 12,
     fontWeight: "bold",
@@ -30,15 +28,31 @@ class Counter extends Component {
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.handleIncrement()} //used when passing parameters. pass the parameter inside the ()
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrement
+        </button>
+        <div>{this.renderTags()}</div>
       </React.Fragment>
     );
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
